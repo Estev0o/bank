d3962a8600c5ea1085bf291bb103a10a309f95bc
Estevo Vinicius <viniestevo33@gmail.com>
2023-09-19 15:32:14 -0300

    validation CPF done

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,11 @@
 export default function ehumCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g,"");
-    validaNumerosRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
-    console.log(validaNumerosRepetidos(cpf));
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("esse cpf nao existe")
+    }
+    else {
+        console.log("CPF existe!")
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -38,5 +41,24 @@ if(soma == 10 || soma == 11) {
 }
 
 return soma != cpf[9];
+
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+    
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador ;
+        multiplicador --;
+    }
+
+soma = (soma * 10) % 11;
+
+if(soma == 10 || soma == 11) {
+    soma = 0
+}
+
+return soma != cpf[10];
 
 }

5b79522f9e96119e7a20d50e2f970eedc6e5ac93
Estevo Vinicius <viniestevo33@gmail.com>
2023-10-01 15:31:26 -0300

    Function take picture

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,11 @@
 const botaoInicarCamera = document.querySelector("[ data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
+const botaoTirar = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+
+let imagemURL = "";
 
 botaoInicarCamera.addEventListener("click", async function (){
     const iniciarVideo = await navigator.mediaDevices.
@@ -10,4 +15,13 @@ botaoInicarCamera.addEventListener("click", async function (){
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
+})
+
+botaoTirar.addEventListener("click", function(){
+    canvas.getContext('2d').drawImage(video,0,0, canvas.Width, canvas.heigth)
+
+    imagemURL = canvas.toDataURL ("image/jpeg");
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block";
 })
